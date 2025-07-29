b3435b451eff24f362fedee56a804f23df6e42c9
souravgituser <sourav.sarkar@codeclouds.in>
2025-07-29 18:34:50 +0530

    react project

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from '../components/header/Header';
-import Footer from '../components/footer/footer';
+import Footer from '../components/footer/Footer';
 
 const MainLayout = () => {
   return (
